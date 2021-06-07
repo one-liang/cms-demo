e491cbb69a4ca2fcafb1c54339860f0cd414a275
Liang <william810212@gmail.com>
2021-06-07 15:27:28 +0800

    chore: add api path

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,16 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+export default {
+  created() {
+    console.log(process.env.VUE_APP_API, process.env.VUE_APP_PATH);
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
