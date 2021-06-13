64822418d8095760bae8a8624802843f9b40a23f
Liang <william810212@gmail.com>
2021-06-13 16:01:59 +0800

    chore: add vee-validate

--- src/main.js
+++ src/main.js
@@ -4,10 +4,29 @@ import VueAxios from 'vue-axios';
 import Loading from 'vue3-loading-overlay';
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// vee-validate
+import {
+  Form, Field, ErrorMessage, defineRule, configure,
+} from 'vee-validate';
+import AllRules from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import { currency } from './methods/filters';
+
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule]);
+});
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true, // 當輸入任何內容直接進行驗證
+});
+setLocale('zh_TW');
 
 const app = createApp(App);
 
@@ -16,6 +35,9 @@ app.config.globalProperties.$filters = {
 };
 app.use(VueAxios, axios);
 app.component('Loading', Loading);
+app.component('Form', Form);
+app.component('Field', Field);
+app.component('ErrorMessage', ErrorMessage);
 app.use(store);
 app.use(router);
 app.mount('#app');
