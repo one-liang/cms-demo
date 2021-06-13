5ffd8663b7ba1c354ada5545060fe85993c3c4c5
Liang <william810212@gmail.com>
2021-06-13 14:57:13 +0800

    chore: add cart list

--- src/views/user/UserCart.vue
+++ src/views/user/UserCart.vue
@@ -73,6 +73,91 @@
         </table>
       </div>
       <!-- 購物車列表 -->
+      <div class="col-md-5">
+        <div class="sticky-top">
+          <table class="table align-middle">
+            <thead>
+              <tr>
+                <th></th>
+                <th>品名</th>
+                <th style="width: 110px">數量</th>
+                <th>單價</th>
+              </tr>
+            </thead>
+            <tbody>
+              <template v-if="cart.carts">
+                <tr v-for="item in cart.carts" :key="item.id">
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-outline-danger btn-sm"
+                      :disabled="loadingItem === item.id"
+                      @click="removeCartItem(item.id)"
+                    >
+                      <i class="bi bi-trash2-fill"></i>
+                    </button>
+                  </td>
+                  <td>
+                    {{ item.product.title }}
+                    <div class="text-success" v-if="item.coupon">
+                      已套用優惠券
+                    </div>
+                  </td>
+                  <td>
+                    <div class="input-group input-group-sm">
+                      <input
+                        type="number"
+                        class="form-control"
+                        v-model.number="item.qty"
+                      />
+                      <div class="input-group-text">
+                        / {{ item.product.unit }}
+                      </div>
+                    </div>
+                  </td>
+                  <td class="text-end">
+                    <small
+                      v-if="cart.final_total !== cart.total"
+                      class="text-success"
+                      >折扣價：</small
+                    >
+                    {{ $filters.currency(item.final_total) }}
+                  </td>
+                </tr>
+              </template>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="3" class="text-end">總計</td>
+                <td class="text-end">{{ $filters.currency(cart.total) }}</td>
+              </tr>
+              <tr v-if="cart.final_total !== cart.total">
+                <td colspan="3" class="text-end text-success">折扣價</td>
+                <td class="text-end text-success">
+                  {{ $filters.currency(cart.final_total) }}
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+          <div class="input-group mb-3 input-group-sm">
+            <input
+              type="text"
+              class="form-control"
+              v-model="coupon_code"
+              placeholder="請輸入優惠碼"
+            />
+            <div class="input-group-append">
+              <button
+                class="btn btn-outline-secondary"
+                type="button"
+                @click="addCouponCode"
+              >
+                套用優惠碼
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -85,10 +170,13 @@ export default {
       products: [],
       isLoading: false,
       loadingItem: '',
+      cart: {},
+      coupon_code: '',
     };
   },
   created() {
     this.getProducts();
+    this.getCart();
   },
   methods: {
     getProducts() {
@@ -124,6 +212,18 @@ export default {
           }
         })
         .catch((error) => console.log(error));
+    },
+    getCart() {
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/cart`;
+      this.isLoading = true;
+      this.axios.get(api).then((response) => {
+        console.log('getCart: ', response);
+        this.cart = response.data.data;
+        this.isLoading = false;
+      });
+    },
+    removeCartItem(id) {
+      console.log('removeCartItem: ', id);
     },
   },
 };
